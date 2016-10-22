59899bbead129b9cb59e41c5d315cac267bed4ef
ganhong <1137361494@qq.com>
2016-10-22 00:44:30 -0400

    weighted quick union + path compression

---
+++ UnionFind/Code/UnionFind.java
@@ -0,0 +1,40 @@
+package com.hong.algo;
+
+/**
+ * Created by GanHong on 10/22/16.
+ */
+public class UnionFind {
+    public UnionFind(int n) {
+        int[] id = new int[n];
+        int[] sz = new int[n];
+        for(int i=0; i<n; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public boolean find(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    private int root(int i) {
+        for( ; i != id[i]; i = id[i]) {
+            id[i] = id[id[i]];
+        }
+        return i;
+    }
+
+    public void unite(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if(sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
+
+
+}
